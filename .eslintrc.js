d47de4b62c5190631be1d9e1557a918c324be6a5
Artem <mstrjesm@gmail.com>
2023-07-23 22:05:05 +0300

    fix: translation rule for tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,10 +41,20 @@ module.exports = {
     'comma-dangle': 'off',
     'arrow-body-style': 'off',
     'linebreak-style': 'off',
-    'i18next/no-literal-string': ['error', { markupOnly: true }],
+    'i18next/no-literal-string': [
+      'error', { markupOnly: true, ignoreAttribute: ['data-testid'] }
+    ],
     'max-len': ['error', { ignoreComments: true, code: 100 }]
   },
   globals: {
     __IS_DEV__: true,
   },
+  overrides: [
+    {
+      files: ['**/src/**/*.test.{ts,tsx}'],
+      rules: {
+        'i18next/no-literal-string': 'off'
+      }
+    }
+  ]
 };
