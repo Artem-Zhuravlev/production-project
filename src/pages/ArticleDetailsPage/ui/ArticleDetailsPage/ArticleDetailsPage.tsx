7c426177721ffabfb0e5f73ce174939e6eecad2b
Artem <mstrjesm@gmail.com>
2024-01-13 14:06:51 +0200

    feat: add skeleton, code article details card

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -1,6 +1,8 @@
 import { classNames } from 'shared/lib/classNames/ClassNames';
 import { useTranslation } from 'react-i18next';
 import { memo } from 'react';
+import { useParams } from 'react-router-dom';
+import { ArticleDetails } from 'entities/Article';
 import cls from './ArticleDetailsPage.module.scss';
 
 interface ArticleDetailsPageProps {
@@ -9,10 +11,23 @@ interface ArticleDetailsPageProps {
 
 const ArticleDetailsPage = ({ className }: ArticleDetailsPageProps) => {
   const { t } = useTranslation('article');
+  const { id } = useParams();
+
+  if (!id) {
+    return (
+      <div
+        className={classNames(cls.ArticleDetailsPage, {}, [className])}
+      >
+        {t('article_not_found')}
+      </div>
+    );
+  }
 
   return (
-    <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>
-      {t('article__details_page')}
+    <div
+      className={classNames(cls.ArticleDetailsPage, {}, [className])}
+    >
+      <ArticleDetails id={id} />
     </div>
   );
 };
