4a0e2b452e56b4ca5f7726f802d7b06bfa0efdff
Artem <mstrjesm@gmail.com>
2024-01-20 21:13:13 +0200

    fix: axios config, remove mock for async pages

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.async.ts
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.async.ts
@@ -1,8 +1,4 @@
 import { FC, lazy } from 'react';
 import { LoginFormProps } from './LoginForm';
 
-export const LoginFormAsync = lazy <FC<LoginFormProps>>(() => new Promise((resolve) => {
-  // @ts-ignore
-  // mock file for education
-  setTimeout(() => resolve(import('./LoginForm')), 300);
-}));
+export const LoginFormAsync = lazy <FC<LoginFormProps>>(() => import('./LoginForm'));
--- src/features/addCommentForm/ui/AddCommentForm/AddCommentForm.async.ts
+++ src/features/addCommentForm/ui/AddCommentForm/AddCommentForm.async.ts
@@ -1,6 +1,3 @@
 import { lazy } from 'react';
 
-export const AddCommentFormAsync = lazy(() => new Promise((resolve) => {
-  // @ts-ignore
-  setTimeout(() => resolve(import('./AddCommentForm')), 1500);
-}));
+export const AddCommentFormAsync = lazy(() => import('./AddCommentForm'));
