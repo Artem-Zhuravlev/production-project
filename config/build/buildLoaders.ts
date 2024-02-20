d4ebf392277eb0d530ca63497775474a83e5f1f2
Artem <mstrjesm@gmail.com>
2024-02-20 20:10:40 +0200

    feat: add plugin for removing unused attrs from bundle

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -11,14 +11,9 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
     use: ['@svgr/webpack'],
   };
 
+  const codeBabelLoader = buildBabelLoader({ ...options, isTsx: false });
+  const tsxCodeBabelLoader = buildBabelLoader({ ...options, isTsx: true });
   const cssLoader = buildCssLoader(isDev);
-  const babelLoader = buildBabelLoader(options);
-
-  const typescriptLoader = {
-    test: /\.tsx?$/,
-    use: 'ts-loader',
-    exclude: /node_modules/,
-  };
 
   const fileLoader = {
     test: /\.(png|jpe?g|gif|woff2|woff)$/i,
@@ -32,8 +27,8 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
   return [
     fileLoader,
     svgLoader,
-    babelLoader,
-    typescriptLoader,
+    codeBabelLoader,
+    tsxCodeBabelLoader,
     cssLoader,
   ];
 }
