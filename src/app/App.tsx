e7fa6cb9b50df0e7f6ca5f7121155ae55b7a4cb4
Artem <mstrjesm@gmail.com>
2024-02-26 13:55:36 +0200

    feat: init auth data from server

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,21 @@
 import { Suspense, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { Navbar } from '@/widgets/Navbar';
 import { Sidebar } from '@/widgets/Sidebar';
 
 import { useTheme } from '@/shared/lib/hooks/useTheme/useTheme';
 import { classNames } from '@/shared/lib/classNames/ClassNames';
-import { getUserInited, userActions } from '@/entities/User';
+import { getUserInited, initAuthData } from '@/entities/User';
 import { AppRouter } from './providers/router';
+import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
 
 const App = () => {
   const { theme } = useTheme();
-  const dispatch = useDispatch();
+  const dispatch = useAppDispatch();
   const inited = useSelector(getUserInited);
 
   useEffect(() => {
-    dispatch(userActions.initAuthData());
+    dispatch(initAuthData());
   }, [dispatch]);
 
   return (
--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -9,7 +9,7 @@ export {
 } from './model/selectors/roleSelectors';
 
 export { userReducer, userActions } from './model/slice/userSlice';
-
+export { initAuthData } from './model/slice/initAuthData';
 export type { UserSchema, User } from './model/types/user';
 export { UserRole } from './model/consts/consts';
 export { useJsonSettings } from './model/selectors/jsonSettings';
